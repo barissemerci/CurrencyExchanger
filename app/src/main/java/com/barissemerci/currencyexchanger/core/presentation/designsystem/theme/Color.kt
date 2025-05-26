65d68baeef863cdc06a1ab8a517845d02afd51ab
Barış SEMERCİ <barissemerci3@gmail.com>
2025-05-26 16:11:35 +0300

    update theme colors

--- app/src/main/java/com/barissemerci/currencyexchanger/core/presentation/designsystem/theme/Color.kt
+++ app/src/main/java/com/barissemerci/currencyexchanger/core/presentation/designsystem/theme/Color.kt
@@ -1,11 +1,17 @@
 package com.barissemerci.currencyexchanger.core.presentation.designsystem.theme
 
+import androidx.compose.material3.ColorScheme
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Primary = Color(0xFF459BD8)
+val PrimaryGradientStart = Color(0xFF3B82C1)
+val PrimaryGradientEnd = Color(0xFF3B99D8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val SellRed = Color(0xFFDF5D58)
+val ReceiveGreen = Color(0xFF499D72)
+
+val ColorScheme.buttonGradient: Brush
+    get() = Brush.linearGradient(
+        colors = listOf(PrimaryGradientStart, PrimaryGradientEnd)
+    )
