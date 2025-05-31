e8a7dc2d1c8077988a779e926683ce7a969f37ce
Barış SEMERCİ <barissemerci3@gmail.com>
2025-05-31 11:34:27 +0300

    remove OnDismissBuyCurrencyList from Actions

--- app/src/main/java/com/barissemerci/currencyexchanger/exchanger/presentation/currency_exchange/ExchangerAction.kt
+++ app/src/main/java/com/barissemerci/currencyexchanger/exchanger/presentation/currency_exchange/ExchangerAction.kt
@@ -4,7 +4,6 @@ package com.barissemerci.currencyexchanger.exchanger.presentation.currency_excha
 sealed interface ExchangerAction {
     data object OnSubmit : ExchangerAction
     data object OnClickChangeBuyCurrency : ExchangerAction
-    data object OnDismissBuyCurrencyList : ExchangerAction
 
     data class OnChangeSellAmount(val amount: String) : ExchangerAction
     data class OnChangeBuyCurrency(val currencyIndex: Int) : ExchangerAction
