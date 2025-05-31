0648fbae13c450c2fbd678dbe01e9187039cde4b
Barış SEMERCİ <barissemerci3@gmail.com>
2025-05-31 14:46:28 +0300

    add mutex and non cancellable to ConvertCurrencyUseCase

--- app/src/main/java/com/barissemerci/currencyexchanger/core/domain/util/ExchangeError.kt
+++ app/src/main/java/com/barissemerci/currencyexchanger/core/domain/util/ExchangeError.kt
@@ -1,5 +1,6 @@
 package com.barissemerci.currencyexchanger.core.domain.util
 
 enum class ExchangeError : Error {
-    NOT_ENOUGH_BALANCE
+    NOT_ENOUGH_BALANCE,
+    UNKNOWN
 }
--- app/src/main/java/com/barissemerci/currencyexchanger/exchanger/domain/exchange_usecase/ExchangeUseCase.kt
+++ app/src/main/java/com/barissemerci/currencyexchanger/exchanger/domain/exchange_usecase/ExchangeUseCase.kt
@@ -6,15 +6,20 @@ import com.barissemerci.currencyexchanger.exchanger.domain.available_balance.Ava
 import com.barissemerci.currencyexchanger.exchanger.domain.commission_rule.CommissionRuleManager
 import com.barissemerci.currencyexchanger.exchanger.domain.commission_rule.CommissionRuleType
 import com.barissemerci.currencyexchanger.exchanger.domain.exchange_count.ExchangeCountDataSource
+import kotlinx.coroutines.NonCancellable
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.withContext
 import java.math.BigDecimal
 import java.math.RoundingMode
+import kotlin.coroutines.cancellation.CancellationException
 
 class ConvertCurrencyUseCase(
     private val exchangeCountDataSource: ExchangeCountDataSource,
     private val availableBalanceDataSource: AvailableBalanceDataSource
 ) {
-
+    private val mutex = Mutex()
 
     suspend operator fun invoke(
         exchangeRate: BigDecimal,
@@ -22,45 +27,57 @@ class ConvertCurrencyUseCase(
         toCurrency: String,
         amount: BigDecimal,
         ruleType: CommissionRuleType
-    ): Result<ConversionResult, ExchangeError> {
+    ): Result<ConversionResult, ExchangeError> = mutex.withLock {
+        try {
+            val fromBalance =
+                availableBalanceDataSource.getBalance(fromCurrency)?.currencyAmount
+                    ?: BigDecimal.ZERO
+            val toBalance =
+                availableBalanceDataSource.getBalance(toCurrency)?.currencyAmount ?: BigDecimal.ZERO
 
-        val fromBalance =
-            availableBalanceDataSource.getBalance(fromCurrency)?.currencyAmount ?: BigDecimal.ZERO
-        val toBalance =
-            availableBalanceDataSource.getBalance(toCurrency)?.currencyAmount ?: BigDecimal.ZERO
+            val ruleManager = CommissionRuleManager()
+            val exchangeCount = exchangeCountDataSource.exchangeCount.first()
 
-        val ruleManager = CommissionRuleManager()
-        val exchangeCount = exchangeCountDataSource.exchangeCount.first()
+            val commission = ruleManager.calculateCommission(
+                ruleType = ruleType,
+                conversionCount = exchangeCount + 1,
+                amount = amount
+            ).setScale(2, RoundingMode.HALF_EVEN)
 
-        val commission = ruleManager.calculateCommission(
-            ruleType = ruleType,
-            conversionCount = exchangeCount + 1,
-            amount = amount
-        ).setScale(2, RoundingMode.HALF_EVEN)
+            val totalCost = amount.add(commission)
 
-        val totalCost = amount.add(commission)
+            if (fromBalance < totalCost) {
+                return Result.Error(ExchangeError.NOT_ENOUGH_BALANCE)
+            }
 
-        if (fromBalance < totalCost) {
-            return Result.Error(ExchangeError.NOT_ENOUGH_BALANCE)
+            val convertedAmount = amount.multiply(exchangeRate)
+
+            withContext(NonCancellable) {
+                availableBalanceDataSource.updateBalance(
+                    fromCurrency,
+                    fromBalance.subtract(totalCost)
+                )
+                availableBalanceDataSource.updateBalance(toCurrency, toBalance.add(convertedAmount))
+                exchangeCountDataSource.incrementExchangeCount()
+            }
+
+            return Result.Success(
+                ConversionResult(
+                    fromCurrency = "EUR",
+                    toCurrency = toCurrency,
+                    sellAmount = amount,
+                    buyAmount = convertedAmount,
+                    rate = exchangeRate,
+                    commissionFee = commission,
+                    totalDeducted = totalCost
+                )
+            )
+        } catch (e: Exception) {
+            if (e is CancellationException) {
+                throw e
+            }
+            return Result.Error(ExchangeError.UNKNOWN)
         }
 
-        val convertedAmount = amount.multiply(exchangeRate)
-
-        availableBalanceDataSource.updateBalance(fromCurrency, fromBalance.subtract(totalCost))
-        availableBalanceDataSource.updateBalance(toCurrency, toBalance.add(convertedAmount))
-
-        exchangeCountDataSource.incrementExchangeCount()
-
-        return Result.Success(
-            ConversionResult(
-                fromCurrency = "EUR",
-                toCurrency = toCurrency,
-                sellAmount = amount,
-                buyAmount = convertedAmount,
-                rate = exchangeRate,
-                commissionFee = commission,
-                totalDeducted = totalCost
-            )
-        )
     }
 }
