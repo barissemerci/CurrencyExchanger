5cf1a964b8ac3b9e64a8123e6188586492650422
Barış SEMERCİ <barissemerci3@gmail.com>
2025-06-01 11:50:06 +0300

    update theme

--- app/src/main/java/com/barissemerci/currencyexchanger/core/presentation/designsystem/theme/Theme.kt
+++ app/src/main/java/com/barissemerci/currencyexchanger/core/presentation/designsystem/theme/Theme.kt
@@ -1,9 +1,7 @@
 package com.barissemerci.currencyexchanger.core.presentation.designsystem.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
 
@@ -20,31 +18,14 @@ private val DarkColorScheme = darkColorScheme(
     onSurface = White,
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Green,
-    secondary = Gray,
-    tertiary = LightGray,
-    background = Black,
-    surface = DarkGray,
-    onPrimary = White,
-    onSecondary = White,
-    onTertiary = White,
-    onBackground = White,
-    onSurface = White,
-)
 
 @Composable
 fun CurrencyExchangerTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
 
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = DarkColorScheme,
         content = content
     )
 }
